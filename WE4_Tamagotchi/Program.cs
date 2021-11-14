18e8e8298b53e9fb7b39abd4d438042064465e9a
Bart Tassignon <bart.tassignon@student.ehb.be>
2021-11-14 14:45:29 +0100

    Add project files.

---
+++ WE4_Tamagotchi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Timers;
+
+namespace WE4_Tamagotchi
+{
+
+    class Program
+    {
+        private static System.Timers.Timer myTimer;
+        static void Main(string[] args)
+        {
+            SetTimer();
+
+            Console.WriteLine("\nPress Enter to exit the application... \n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            Console.ReadLine();
+            myTimer.Stop();
+            myTimer.Dispose();
+
+
+
+        }
+
+        public static void SetTimer()
+        {
+            myTimer = new System.Timers.Timer(1000);
+            myTimer.Elapsed += OnTimedEvent;
+            myTimer.AutoReset = true;
+            myTimer.Enabled = true;
+
+        }
+
+        public static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("De tijd is {0:HH:mm:ss.fff},", e.SignalTime); ;
+        }
+    }
+}
---
+++ WE4_Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Timers;
+
+namespace WE4_Tamagotchi
+{
+    enum Levensstadium
+    {
+        Ei,
+        Baby,
+        Kind,
+        Puber,
+        Volwassen,
+        Senior,
+        Dood
+    }
+
+    class Tamagotchi
+    {
+        // Deel 3: events
+        private readonly Timer timer;
+        private Levensstadium vorigLevensstatium;
+
+        // Deel 1
+        public string Naam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+
+        public Levensstadium levensstadium;
+
+        private int honger;
+        public int Honger
+        {
+            get { return honger; }
+            set
+            {
+                if (value >= 0 && value <= 4)
+                {
+                    honger = value;
+                }
+            }
+        }
+
+        private int geluk;
+        public int Geluk
+        {
+            get { return geluk; }
+            set
+            {
+                if (value >= 0 && value <= 4)
+                {
+                    geluk = value;
+                }
+            }
+        }
+
+        private int intelligentie;
+        public int Intelligentie
+        {
+            get { return intelligentie; }
+            set
+            {
+                if (value >= 0 && value <= 4)
+                {
+                    intelligentie = value;
+                }
+            }
+        }
+        // Deel 2
+        public Tamagotchi(string naam = "Beestje")
+        {
+            Naam = naam;
+            Geboortedatum = DateTime.Now;
+            Honger = 4;
+            Geluk = 4;
+            Intelligentie = 4;
+
+        }
+
+    }
+}
